d4daf16b811a0ce9cd8185d796ce2b24b2c0189a
Samuel Lima <samuelvinicius433@gmail.com>
2022-06-07 20:38:27 -0300

    Condições
    Condições aninhadas
    IF and Else
    Switch

---
+++ Aula11,12/ex08.js
@@ -0,0 +1,7 @@
+var vel = 20
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+if (vel > 60) { //Condição Simples
+    console.log('Você ultrapassou o limite de velocidade. MULTADO!')
+
+}
+console.log('Dirija sempre utilizando cinto de segurança!')
